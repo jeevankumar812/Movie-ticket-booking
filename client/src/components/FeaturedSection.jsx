a57fd98e49a25f75dee1436b3ea0513c6ba40dc3
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 22:08:08 +0530

    use Navige to movies

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -7,7 +7,8 @@ const FeaturedSection = () => {
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44
     overflow-hidden'>
-      <div>
+      <div className='relative flex items-center justify-between
+      pt-20 pb-10'>
         <p className='=text-gray-300 font-medium text-lg'>Now Showing </p>
         <button onClick={()=>navigate('/movies')}className='px-4 py-1 sm:px-7 sm:py-2 bg-cyan-500 hover:bg-cyan-400 
         text-white font-medium rounded-full cursor-pointer transition-all duration-300 transform hover:scale-105 shadow-md hover:shadow-cyan-400/60'>

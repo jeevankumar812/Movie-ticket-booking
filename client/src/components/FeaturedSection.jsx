2c51cb8d18e1eb4d42e9945b60e076d0c7e213be
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 22:04:38 +0530

    button property

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -7,7 +7,9 @@ const FeaturedSection = () => {
     overflow-hidden'>
       <div>
         <p>Now Showing </p>
-        <button>View All
+        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-cyan-500 hover:bg-cyan-400 
+        text-white font-medium rounded-full cursor-pointer transition-all duration-300 transform hover:scale-105 shadow-md hover:shadow-cyan-400/60'>
+            View All
              <ArrowRight className='=group-hover:translate-x-0.5 transition w-4.5 h-4.5'/>
              </button>
       </div>

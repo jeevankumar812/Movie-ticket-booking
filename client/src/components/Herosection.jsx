ef9471346d3f80ec0138d51bacb66fd4453330ba
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 21:41:03 +0530

    Added bg image

--- client/src/components/Herosection.jsx
+++ client/src/components/Herosection.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { assets } from '../assets/assets';
+import { Calendar, Clock } from 'lucide-react'; 
 
 const Herosection = () => {
   return (
     <div className='flex flex-col items-start justify-center gap-4
-      px-6 md:px-16 lg:px-36 bg-[url("/jk.jpg")]
+      px-6 md:px-16 lg:px-36 bg-[url("/backgroundImage.png")]
       bg-cover bg-center h-screen'>
 
     <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20" />
@@ -12,6 +13,20 @@ const Herosection = () => {
     <h1 className='text-5xl md:text-[70px] md:leading-[80px] font-semibold max-w-[1100px]'>
         Guardians <br /> of the Galaxy
       </h1>
+    
+    <div className='flex items-center gap-4 text-gray-300'>
+        <span>Action | Adventure | Sci-Fi</span>
+
+        <div className='flex items-center gap-1'>
+          <Calendar className='w-4 h-4' /> 2018
+        </div>
+
+        <div className='flex items-center gap-1'>
+          <Clock className='w-4 h-4' /> 2h 16m
+        </div>
+      </div>
+
+      
     </div>
 
   );

5bca095183feb9f66d73c684ea635f72460b761c
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 16:32:01 +0530

    Small Screen prolem Bug

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,13 +12,11 @@ const Navbar = () => {
         <img src={assets.logo} alt="" className='w-36 h-auto' />
       </Link>
 
-      <div className={`
-        max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 
-        flex flex-col md:flex-row items-center max-md:justify-center gap-8 px-8 py-3 max-md:h-screen 
-        md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 
-        transition-[width] duration-300 overflow-hidden
-        ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}
-      `}>
+      <div className={ `max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 
+  flex flex-col md:flex-row items-center max-md:justify-center gap-8 px-8 py-3 
+  max-md:h-screen md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 
+  md:border border-gray-300/20 transition-all duration-300 overflow-hidden
+  ${isOpen ? 'max-md:w-full' : 'max-md:w-0 max-md:opacity-0 max-md:pointer-events-none}'}`}>
         {/* Close Icon */}
         <XIcon
           className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'

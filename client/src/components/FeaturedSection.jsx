9dde03aafd037e04b57f2d87f4e2b84a5da60e3a
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 22:03:25 +0530

    Fetured Sction coding

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -1,10 +1,18 @@
+import { ArrowRight } from 'lucide-react'
 import React from 'react'
 
 const FeaturedSection = () => {
   return (
     <div className='px-6 md:px-16 lg:px-24 xl:px-44
     overflow-hidden'>
-      
+      <div>
+        <p>Now Showing </p>
+        <button>View All
+             <ArrowRight className='=group-hover:translate-x-0.5 transition w-4.5 h-4.5'/>
+             </button>
+      </div>
+      <div></div>
+      <div></div>
     </div>
   )
 }

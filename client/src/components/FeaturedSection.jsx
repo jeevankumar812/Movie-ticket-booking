a58d9fabd2c27b2c7df4b5d59105b909a980a930
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 22:00:07 +0530

    Add one Jsx file

---
+++ client/src/components/FeaturedSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FeaturedSection = () => {
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-44
+    overflow-hidden'>
+      
+    </div>
+  )
+}
+
+export default FeaturedSection

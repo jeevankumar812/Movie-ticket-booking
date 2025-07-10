76a58e14abd31dbef8d84d52e9658bfb395e0e41
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 13:46:17 +0530

    Navbar changed logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         <img src={assets.logo} alt="" className='w-36 h-auto' />
       </Link>
 
-      <div className={`max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 
+      <div className={`max-md:absolute max-md:top-git0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 
         flex flex-col md:flex-row items-center max-md:justify-center gap-8 min-md:px-8 py-3 max-md:h-screen 
         min-md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 md:border border-gray-300/20 
         overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-md:w-full' : 'max-md:w-0'}`}>

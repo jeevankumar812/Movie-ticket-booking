34004bfba4dfbc39b0f01ad71e1b651c91c0b9f3
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-10 20:10:24 +0530

    Ebvirament variable Inserted

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
-import { SearchIcon, MenuIcon, XIcon } from 'lucide-react';
+import { SearchIcon, MenuIcon, XIcon, TicketPlus } from 'lucide-react';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
+  const { user } = useUser()
+  const { openSignIn } = useClerk()
+  const navigate =useNavigate()
 
   return (
     <div className="fixed top-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
       <Link to='/' className="max-md:flex-1">
-        <img src={assets.logo} alt="" className='w-36 h-auto' />
+        <img src={assets.logo} alt="logo" className='w-36 h-auto' />
       </Link>
 
-      <div className={ `max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 
-  flex flex-col md:flex-row items-center max-md:justify-center gap-8 px-8 py-3 
-  max-md:h-screen md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 
-  md:border border-gray-300/20 transition-all duration-300 overflow-hidden
-  ${isOpen ? 'max-md:w-full' : 'max-md:w-0 max-md:opacity-0 max-md:pointer-events-none}'}`}>
+      <div className={`
+        max-md:absolute max-md:top-0 max-md:left-0 max-md:font-medium max-md:text-lg z-50 
+        flex flex-col md:flex-row items-center max-md:justify-center gap-8 px-8 py-3 
+        max-md:h-screen md:rounded-full backdrop-blur bg-black/70 md:bg-white/10 
+        md:border border-gray-300/20 transition-all duration-300 overflow-hidden
+        ${isOpen ? 'max-md:w-full' : 'max-md:w-0 max-md:opacity-0 max-md:pointer-events-none'}
+      `}>
         {/* Close Icon */}
         <XIcon
           className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'
-          onClick={() => setIsOpen(!isOpen)}
+          onClick={() => setIsOpen(false)}
         />
 
         <Link onClick={() => { scrollTo(0, 0); setIsOpen(false); }} to='/'>Home</Link>
@@ -32,11 +38,22 @@ const Navbar = () => {
 
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer' />
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-cyan-500 hover:bg-cyan-400 text-white font-medium 
-        rounded-full cursor-pointer transition-all duration-300 transform hover:scale-105 shadow-md hover:shadow-cyan-400/60
-'>
-          Login
-        </button>
+        
+        {!user ? (
+          <button
+            onClick={() => openSignIn()}
+            className='px-4 py-1 sm:px-7 sm:py-2 bg-cyan-500 hover:bg-cyan-400 text-white font-medium 
+              rounded-full cursor-pointer transition-all duration-300 transform hover:scale-105 shadow-md hover:shadow-cyan-400/60'>
+            Login
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action label="My Bookings" labelIcon=
+              {<TicketPlus width={15}/>} onClick={()=> navigate('/my-bookings')}/>
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
       </div>
 
       <MenuIcon

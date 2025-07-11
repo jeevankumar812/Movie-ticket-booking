7be26e48d3e69152c80d8152a2cf8f584216e19d
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-11 19:57:16 +0530

    style added

--- client/src/components/BlurCircle.jsx
+++ client/src/components/BlurCircle.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BlurCircle = ({top="auto",left="auto",right="auto",bottom="auto"}) => {
   return (
     <div className="absolute -z-50 h-58 w-50 aspect-square rounded-full bg-primary/30 blur-3x1"
-    style={{top: }}>
+    style={{top: top,left: left,right: right,bottom: bottom}}>
       
     </div>
   )

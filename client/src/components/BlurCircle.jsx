a53b8c99044c943f9828e831a3b515cc8f55404c
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-11 19:56:31 +0530

    BlurCircle File Added

---
+++ client/src/components/BlurCircle.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BlurCircle = ({top="auto",left="auto",right="auto",bottom="auto"}) => {
+  return (
+    <div className="absolute -z-50 h-58 w-50 aspect-square rounded-full bg-primary/30 blur-3x1"
+    style={{top: }}>
+      
+    </div>
+  )
+}
+
+export default BlurCircle

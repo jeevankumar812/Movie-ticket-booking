3789c92a0e470f5ecadc1f19e2c9516b29e84b5d
K Jeevan Kumar <jeevanjeevan63643@gmail.com>
2025-07-11 21:58:21 +0530

    Button Hover

--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -18,7 +18,7 @@ const MovieCard = ({movie}) => {
       </p>
 
       <div>
-        <button className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition
+        <button onClick={()=>{navigate(`/movies/${movie._id}`); scrollTo(0,0)}} className='px-4 py-2 text-xs bg-primary hover:bg-primary-dull transition
         rounded-full font-medium cursor-pointer'>Buy Tickets</button>
         <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
             <StarIcon className="w-4 h-4 text-primary fill-primary"/>
